e7bcf716424cb2fb653880f27a4e12bc85a8423e
otoDone <498978473@qq.com>
2026-08-30 18:13:37 +0800

    update

--- tCarCore/toolbox/wlan-helper.sh
+++ tCarCore/toolbox/wlan-helper.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-exec /bin/sh "$(dirname "$0")/../install/wlan-helper.sh"
+set -u
+CONFIG=$(dirname "$0")/../media/network.yaml
+IFACE=$(awk -F: '$1 ~ /^[[:space:]]*interface[[:space:]]*$/ {gsub(/[[:space:]\"]/,"",$2); print $2; exit}' "$CONFIG")
+IFACE=${IFACE:-wlan0}
+exec /sbin/wpa_supplicant -c/etc/wpa_supplicant/wpa_supplicant.conf -Dnl80211,wext -i"$IFACE"
