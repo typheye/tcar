329cf0bcb3e05e918af368bf9bbc566b6bd4df43
otoDone <78747175+typheye@users.noreply.github.com>
2026-09-02 00:25:48 +0800

    update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,19 @@
-pluginManagement { repositories { google(); mavenCentral(); gradlePluginPortal() } }
+pluginManagement {
+    repositories {
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/central")
+        google(); mavenCentral(); gradlePluginPortal()
+    }
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/central")
+        maven("https://maven.aliyun.com/repository/public")
+        google(); mavenCentral()
+    }
 }
-rootProject.name = "tCarKit"
+rootProject.name = "tcar-android"
 include(":app")
